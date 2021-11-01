5393032edcb3ce27f4f3df5d5b9cf6a6176b2665
JM-Lemmi <jm.lemmerich@gmail.com>
2021-11-01 13:29:06 +0100

    Using ENV for credentials but not fully working, see Issue #4

--- files/send_mail.py
+++ files/send_mail.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 import sys
+import os
 from zimbra import Response, ZimbraUser, WebkitAttachment
 
 user = ZimbraUser("https://studgate.dhbw-mannheim.de")
-user.login("s212689", "##")
+user.login(os.getenv('ZIMBRA_USERNAME'), os.getenv('ZIMBRA_PASSWORD'))
 payload, boundary = user.generate_eml_payload(sys.stdin.read())
 user.send_raw_payload(payload, boundary)
