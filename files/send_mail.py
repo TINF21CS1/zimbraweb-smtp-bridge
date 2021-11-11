1fe079e4a833c0b16a87b953392f19468cc87909
Frederik Reiter <kontakt@frederikreiter.de>
2021-11-11 11:34:16 +0100

    Only allow sending as authenticated user.

--- files/send_mail.py
+++ files/send_mail.py
@@ -3,6 +3,7 @@ import sys
 import os
 import pickle
 import logging
+from email.parser import Parser
 
 from zimbraweb import ZimbraUser
 from zimbraweb import emlparsing
@@ -13,6 +14,9 @@ ZIMBRA_USERNAME = sys.argv[3]
 is_bounce = False
 
 user = ZimbraUser("https://studgate.dhbw-mannheim.de")
+raw_eml = sys.stdin.read()
+parser = Parser()
+parsed = parser.parsestr(raw_eml)
 
 if ZIMBRA_USERNAME.strip() == "":
     RECEIVER = sys.argv[2]
@@ -21,7 +25,17 @@ if ZIMBRA_USERNAME.strip() == "":
         exit(0)
     else:
         ZIMBRA_USERNAME = RECEIVER
+        if "MAILER-DAEMON@" not in parsed.get("From"):
+            logging.error("Trying to send non-bounce email to authenticated user.")
+            exit(0)
         is_bounce = True
+        del parsed["From"]
+        parsed["From"] = f"MAILER DAEMON <{RECEIVER}@student.dhbw-mannheim.de>"
+        raw_eml = parsed.as_string()
+
+if  f"<{ZIMBRA_USERNAME}@student.dhbw-mannheim.de>" not in parsed.get("From"):
+    logging.error(f"User {ZIMBRA_USERNAME} tried to send email claiming to be {parsed.get('From')}")
+    exit(0)
 
 failure_code = 1 if not is_bounce else 0 # don't bounce a bounce email or we'll end up in a loop
 
@@ -35,7 +49,6 @@ if not user.authenticated:
     logging.error("User is not authenticated, even though a SASL username exists.")
     exit(failure_code)
 logging.info("Successfully authenticated.")
-raw_eml = sys.stdin.read()
 with open("/srv/zimbraweb/logs/email.eml", "w") as f:
     f.write(raw_eml)
 payload, boundary = emlparsing.parse_eml(user, raw_eml)
