c3825d8fe7a35d21a84d598b66b61ea8f4c185f2
JM-Lemmi <41118534+JM-Lemmi@users.noreply.github.com>
2021-12-24 13:33:26 +0100

    typo in echo

--- files/start.sh
+++ files/start.sh
@@ -9,5 +9,5 @@ echo "$(date +"%b %d %H:%M:%S") $HOSTNAME start.sh[$$]: Started crond."
 python3 /srv/zimbraweb/zimbra_milter.py &
 sleep 2
 echo "$(date +"%b %d %H:%M:%S") $HOSTNAME start.sh[$$]: ➔ Switching to log output from '/var/log/log'"
-echo "$(date +"%b %d %H:%M:%S") $HOSTNAME start.sh[$$]: System is now operational and ready to receive E-Mails" > /var/log/log
+echo "$(date +"%b %d %H:%M:%S") $HOSTNAME start.sh[$$]: System is now operational and ready to receive E-Mails" >> /var/log/log
 tail -f /var/log/log
